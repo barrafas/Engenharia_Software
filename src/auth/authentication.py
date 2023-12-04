aa73686b7fa7ce152ce6df28fb95ad0e4672ff5c
Lucas Treuke <lucasbryantreuke@gmail.com>
2023-12-04 15:22:32 -0300

    feat(auth): adiciona classe e teste unitário para o login do usuário

--- src/auth/authentication.py
+++ src/auth/authentication.py
@@ -1,35 +1,88 @@
-from src.auth.user_management import UserManagement
-from src.database.text_document_module import TextDocumentModule
+"""
+Authentication module
+
+This module is responsible for authenticating users.
+
+TODO:
+    - PEP8 and PEP257 compliance
+    - Refactor code
+    - Add more tests
+    - Add more documentation
+    - Add more type hints
+    - Add more error handling
+    - Add more comments
+    - Add logging
+    - Fix docstrings
+
+"""
+
+
 import bcrypt
+from ..user.user_management import UserManagement
+from ..database.database_module import DatabaseModule
 
 class Authentication:
-    def __init__(self, database_module):
-        # Inicializar o módulo de autenticação
+    """
+    User authentication module
+
+    Attributes:
+        database_module: Database module
+        user_management_module: User management module
+
+    Methods:
+        authenticate_user: Authenticate user
+        verify_password: Verify password
+        logout_user: Logout user
+
+    """
+
+    def __init__(self, database_module: DatabaseModule):
+        """
+        Constructor for Authentication class
+
+        Parameters:
+            database_module: Database module
+        """
         self.database_module = database_module
         self.user_management_module = UserManagement(self.database_module)
 
-    def authenticate_user(self, username, password):
-        # Autenticar o usuário
+    def authenticate_user(self, username: str, password: str) -> bool:
+        """
+        Compare the input password with the hashed password in the database
+
+        Parameters:
+            username: Username - str
+            password: Password - str
+        """
         if not self.user_management_module.user_exists(username):
-            print("\033[31m[ERRO]\033[0m Usuário não encontrado:", username)
+            print(f"Usuário {username} não encontrado!")
             return False
         else:
             user = self.user_management_module.get_user(username)
             user_password = user.get_hashed_password() if user else None
 
             if self.verify_password(password, user_password):
-                print("\033[32m[SUCESSO]\033[0m Autenticação bem-sucedida para o usuário:", username)
+                print("✔ Autenticação bem-sucedida para o usuário:", username)
                 return True
             else:
-                print("\033[31m[ERRO]\033[0m Falha na autenticação para o usuário:", username)
+                print("❌ Falha na autenticação para o usuário:", username)
                 return False
 
-    def verify_password(self, input_password, hashed_password):
-       
-        # Verificar a senha fornecida com a senha armazenada
+    def verify_password(self, input_password: str, hashed_password: str) -> bool:
+        """
+        Verify password
+
+        Parameters:
+            input_password: Input password
+            hashed_password: Hashed password
+        """
         return bcrypt.checkpw(input_password.encode('utf-8'), hashed_password.encode('utf-8'))
 
-    def logout_user(self, username):
-        # Sair do usuário
-        print("\033[32m[SUCESSO]\033[0m Usuário desconectado:", username)
-        return True
+    def logout_user(self, username: str) -> None:
+        """
+        Logout user
+
+        Parameters:
+            username: Username
+        """
+        print("Usuário desconectado:", username)
--- tests/test_auth/test_authentication_module.py
+++ tests/test_auth/test_authentication_module.py
@@ -1,37 +1,73 @@
+"""
+Test the authentication module
+
+TODO:
+    - Review the tests
+    - Review the documentation
+"""
 import unittest
 from src.auth.authentication import Authentication
 from src.database.text_document_module import TextDocumentModule
 from src.auth.user_management import UserManagement
 
+
 class TestAuthenticationModule(unittest.TestCase):
+    """_summary_
+
+    Arguments:
+        unittest -- _description_
+    """
+
     def setUp(self):
-        # Set up for the tests
-        self.database_module_mock = TextDocumentModule("tests/test_auth/test_database.json")
+        """
+        Set up the test environment
+        """
+        self.database_module_mock = TextDocumentModule(
+            "tests/test_auth/test_database.json")
         self.database_module_mock.clear_data()
         self.auth_module = Authentication(self.database_module_mock)
         self.user_management_module = UserManagement(self.database_module_mock)
 
         # Create a test user
-        self.user_management_module.create_user(username="test_user", email="test_email", password="test_password")
+        self.user_management_module.create_user(username="test_user",
+                                                email="test_email", password="test_password")
 
     def test_successful_authentication(self):
-        # Test successful authentication
-        result = self.auth_module.authenticate_user("test_user", "test_password")
+        """
+        GIVEN a user that exists in the database
+        WHEN the user tries to authenticate with the correct password
+        THEN the authentication should be successful
+        """
+        result = self.auth_module.authenticate_user(
+            "test_user", "test_password")
         self.assertTrue(result)
 
     def test_failed_authentication(self):
-        # Test failed authentication
-        result = self.auth_module.authenticate_user("test_user", "wrong_password")
+        """
+        GIVEN a user that exists in the database
+        WHEN the user tries to authenticate with the wrong password
+        THEN the authentication should fail
+        """
+        result = self.auth_module.authenticate_user(
+            "test_user", "wrong_password")
         self.assertFalse(result)
 
     def test_logout_user(self):
-        # Test user logout
-        result = self.auth_module.logout_user("test_user")
-        self.assertTrue(result)
+        """
+        GIVEN a user that is logged in
+        WHEN the user tries to logout
+        THEN the user should be logged out
+        """
+        self.auth_module.authenticate_user("test_user", "test_password")
+        self.auth_module.logout_user("test_user")
+
 
     def tearDown(self):
-        # Clean up after the tests
+        """
+        Clean up after the tests, resets the database
+        """
         self.database_module_mock.clear_data()
 
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main() # pragma: no cover
