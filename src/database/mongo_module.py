207a5aeface3c6dbc1e7160286f958c9e237ab53
GustavoSanches55 <gsanchescosta@yahoo.com.br>
2023-12-07 23:19:19 -0300

    docs(mongodb): adding some documentations to the mongomodule class

--- src/database/mongo_module.py
+++ src/database/mongo_module.py
@@ -2,6 +2,27 @@ import pymongo
 from src.database.database_module import DatabaseModule
 
 class MongoModule(DatabaseModule):
+    """
+    This class implements the DatabaseModule interface for MongoDB.
+
+    Attributes:
+        host (str): The host address of the database.
+        port (int): The port of the database.
+        user (str): The user of the database.
+        password (str): The password of the database.
+        database_name (str): The name of the database.
+        collection_name (str): The name of the collection.
+        client (MongoClient): The MongoClient object.
+        db (Database): The Database object.
+
+    Methods:
+        connect: Connects to the database.
+        disconnect: Disconnects from the database.
+        insert_data: Inserts data into the database.
+        delete_data: Deletes data from the database.
+        update_data: Updates data in the database.
+        select_data: Selects data from the database.
+    """
     def __init__(self, 
                  host: str, 
                  port: int, 
@@ -9,6 +30,17 @@ class MongoModule(DatabaseModule):
                  collection_name: str,
                  user: str = None,
                  password: str = None,):
+        """
+        Constructor method.
+
+        Args:
+            host (str): The host address of the database.
+            port (int): The port of the database.
+            database_name (str): The name of the database.
+            collection_name (str): The name of the collection.
+            user (str): The user of the database.
+            password (str): The password of the database.
+        """
         self.host = host
         self.port = port
         self.user = user
@@ -20,6 +52,11 @@ class MongoModule(DatabaseModule):
         self.collection = None
 
     def connect(self):
+        """
+        Connect to the database.
+        Raises:
+            Exception: If already connected to the database.
+        """
         if self.client:
             raise Exception("Already connected to the database.")
         
@@ -28,6 +65,11 @@ class MongoModule(DatabaseModule):
         self.collection = self.db[self.collection_name]
         
     def disconnect(self):
+        """
+        Disconnect from the database.
+        Raises:
+            Exception: If not connected to the database.
+        """
         if not self.client:
             raise Exception("Not connected to the database.")
         self.client = None
@@ -35,18 +77,54 @@ class MongoModule(DatabaseModule):
         self.collection = None
 
     def insert_data(self, data):
+        """
+        Execute a database query.
+        
+        Args:
+            query (str): The query to execute.
+            
+        Raises:
+            Exception: If not connected to the database.
+        """
         if not self.client:
             raise Exception("Not connected to the database.")
         self.collection.insert_one(data)
 
     def delete_data(self, data):
+        """
+        Delete data from the database.
+
+        Args:
+            data (dict): The data to delete.
+
+        Raises:
+            Exception: If not connected to the database.
+        """
         self.collection.delete_one(data)
 
     def update_data(self, where, data):
+        """
+        Update data in the database.
+
+        Args:
+            where (dict): The data to update.
+            data (dict): The data to update to.
+
+        Raises:
+            Exception: If not connected to the database.
+        """
         self.collection.update_one(where, data)
 
-
     def select_data(self, query=None):
+        """
+        Fetch data from the database.
+
+        Args:
+            query (dict): The query to execute.
+
+        Returns:
+            list: The result of the query.
+        """
         result = list(self.collection.find(query))
 
         return result
