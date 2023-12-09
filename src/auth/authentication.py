011859fae34c0bc6dbeb275a2fcb5348bbbf8af2
Lucastreuke <lucasbryantreuke@gmail.com>
2023-12-09 20:56:54 -0300

    feat(auth): Successfull authentication of a user

--- src/auth/authentication.py
+++ src/auth/authentication.py
@@ -1,12 +1,15 @@
 from ..database.database_module import DatabaseModule
+from ..user.user_management import UserManagement
 
-class Authentication:
+class AuthenticationModule:
 
     def __init__(self, database_module: DatabaseModule):
-        ...
+        self.database_module = database_module
+        self.user_management_module = UserManagement(self.database_module)
 
     def authenticate_user(self, username: str, password: str) -> bool:
-        ...
+        user = self.user_management_module.get_user(username)
+        return self.verify_password(password, user.get_hashed_password())
 
     def verify_password(self, input_password: str, hashed_password: str) -> bool:
         ...
