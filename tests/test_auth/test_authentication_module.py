13e71e4845fe1ff6ab80421b231fd9ca013d4c14
Lucastreuke <lucasbryantreuke@gmail.com>
2023-12-09 20:53:19 -0300

    test(auth): Success when autenticating user

---
+++ tests/test_auth/test_authentication_module.py
@@ -0,0 +1,38 @@
+
+"""
+Test the authentication module
+
+TODO:
+    - Review the tests
+    - Review the documentation
+"""
+import unittest
+from unittest.mock import MagicMock
+from src.auth.authentication import AuthenticationModule
+
+class TestAuthenticationModule(unittest.TestCase):
+    def setUp(self):
+        # You may want to mock the database module for testing
+        self.database_module_mock = MagicMock()
+        self.auth_module = AuthenticationModule(self.database_module_mock)
+
+    def test_authenticate_user_success(self):
+        # Mocking user_exists and get_user methods
+        self.auth_module.user_management_module.user_exists = MagicMock(return_value=True)
+        self.auth_module.user_management_module.get_user = MagicMock(return_value=self.create_mock_user())
+
+        # Mocking verify_password method
+        self.auth_module.verify_password = MagicMock(return_value=True)
+
+        # Test
+        result = self.auth_module.authenticate_user("test_user", "test_password")
+        self.assertTrue(result)
+
+    def create_mock_user(self):
+        # You may want to create a mock user for testing purposes
+        user = MagicMock()
+        user.get_hashed_password = MagicMock(return_value=b'$2b$12$4PVzqGrnWdUzWd9s/VoI2u6cfTS58zvVqEzUzTijp8usZbRAnkE/W')
+        return user
+
+if __name__ == '__main__':
+    unittest.main()
