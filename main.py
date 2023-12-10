f09b58019dde20bf12008fc0081574584ffd1d51
GustavoSanches55 <gsanchescosta@yahoo.com.br>
2023-12-10 11:35:17 -0300

    feat(mongodb): Starting main file

---
+++ main.py
@@ -0,0 +1,19 @@
+from src.app.application import Application
+from src.auth.authentication import AuthenticationModule
+from src.app.ui import TkinterUI
+from src.app.app_states import SignInUp
+from src.database.mongo_module import MongoModule
+# from src.app.states.sign_in_up_state import SignInUp
+
+if __name__ == '__main__':
+    db = MongoModule(host="localhost", 
+                    port=27017,
+                    database_name="test_app")
+    app = Application(db=db)
+
+    initial_state = SignInUp(app)
+    ui = TkinterUI(app)
+    app._ui = ui
+    app.transition_to(initial_state)
+    app.run()
+   
