6234989e92c5caed410b7c5bafb02ded1baa3fa8
Lucastreuke <lucasbryantreuke@gmail.com>
2023-12-10 12:15:12 -0300

    feat(schedule): Schedule manager correctly reacting to changes on Schedules

--- src/schedule/schedule_management.py
+++ src/schedule/schedule_management.py
@@ -3,7 +3,7 @@ from src.database.mongo_module import MongoModule
 #from src.user.user_model import User
 #from src.user.user_management import UserManagement
 from tests.test_schedule.mocks import Element, ElementManagement, User, UserManagement
-
+from .schedule_observer import Observer, Subject
 
 class EmptyPermissionsError(Exception):
     """Raised when the permissions list is empty"""
@@ -17,7 +17,7 @@ class NonExistentIDError(Exception):
     """Raised when the ID does not exist"""
     pass
 
-class ScheduleManagement:
+class ScheduleManagement(Observer):
     """
     ScheduleManagement class
     Responsible for managing the schedules in the database
@@ -211,4 +211,12 @@ class ScheduleManagement:
             element_manager.update_element(element_id)
         else: 
             raise DuplicatedIDError(f"Element with ID {element_id} already exists in schedule {schedule_id}")
-        
+
+    def update(self, schedule: Schedule) -> None:
+        """
+        Called when the schedule is updated.
+
+        Args:
+            schedule: The schedule that was updated.
+        """
+        self.update_schedule(schedule.id)
