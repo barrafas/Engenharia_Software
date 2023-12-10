15a7f29d5dd5fd3bb0f4355dfbc42b4bc8597c1b
Lucastreuke <lucasbryantreuke@gmail.com>
2023-12-10 20:14:55 -0300

    feat(database): Decorator with windows/unix support

--- src/database/utils.py
+++ src/database/utils.py
@@ -1,7 +1,7 @@
-import signal
 import functools
+import os
+import platform
 from src.database.database_module import DatabaseModule
-
 
 class TimeExceedError(Exception):
     pass
@@ -10,27 +10,63 @@ def timeout(seconds, error_message="Timeout"):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            def handler(signum, frame):
-                raise TimeExceedError(error_message)
+            result = None
+            exception = None
 
-            # Set the signal handler and timeout
-            signal.signal(signal.SIGALRM, handler)
-            signal.alarm(seconds)
+            # Check the operating system and import the appropriate module
+            if platform.system() == "Windows":
+                import threading
 
-            try:
-                result = func(*args, **kwargs)
-            finally:
-                # Reset the alarm
-                signal.alarm(0)
+                def worker():
+                    nonlocal result, exception
+                    try:
+                        result = func(*args, **kwargs)
+                    except Exception as e:
+                        exception = e
 
-            return result
+                thread = threading.Thread(target=worker)
+                thread.start()
+                thread.join(seconds)
+
+                if thread.is_alive():
+                    raise TimeExceedError(error_message)
+                elif exception is not None:
+                    raise exception
+
+                return result
+
+            else:
+                from signal import signal
+
+                def handler(signum, frame):
+                    raise TimeExceedError(error_message)
+
+                def worker():
+                    nonlocal result, exception
+                    try:
+                        result = func(*args, **kwargs)
+                    except Exception as e:
+                        exception = e
+
+                signal.signal(signal.SIGALRM, handler)
+                signal.alarm(seconds)
+
+                try:
+                    worker()
+                finally:
+                    signal.alarm(0)
+
+                if exception is not None:
+                    raise exception
+
+                return result
 
         return wrapper
 
     return decorator
 
 class TimeoutDecorator(DatabaseModule):
-    def __init__(self, decorated, timeout_seconds=5):
+    def __init__(self, decorated, timeout_seconds=10):
         self._decorated = decorated
         self._timeout_seconds = timeout_seconds
 
