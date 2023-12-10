877e1e6a717f6ddd193e925826debcb277798a52
Lucastreuke <lucasbryantreuke@gmail.com>
2023-12-09 21:20:30 -0300

    feat(auth): Error when authenticating a non existing user correctly handled

--- src/auth/authentication.py
+++ src/auth/authentication.py
@@ -2,6 +2,10 @@ from ..database.database_module import DatabaseModule
 from ..user.user_management import UserManagement
 import bcrypt
 
+# create a custom exception
+class UserNotFound(Exception):
+    pass
+
 class AuthenticationModule:
 
     def __init__(self, database_module: DatabaseModule):
@@ -9,8 +13,16 @@ class AuthenticationModule:
         self.user_management_module = UserManagement(self.database_module)
 
     def authenticate_user(self, username: str, password: str) -> bool:
-        user = self.user_management_module.get_user(username)
-        return self.verify_password(password, user.get_hashed_password())
+        if not self.user_management_module.user_exists(username):
+            raise UserNotFound(f"Usuário {username} não encontrado!")
+        else:
+            user = self.user_management_module.get_user(username)
+            user_password = user.get_hashed_password()
+
+            if self.verify_password(password, user_password):
+                return True
+            else:
+                return False
 
     def verify_password(self, input_password: str, hashed_password: str) -> bool:
         return bcrypt.checkpw(input_password.encode(), hashed_password.encode())
