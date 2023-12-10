935a899f9f54ff3db91ab357bbc69105507d2763
Lucastreuke <lucasbryantreuke@gmail.com>
2023-12-09 21:06:16 -0300

    test(auth): Correct password verification

--- tests/test_auth/test_authentication_module.py
+++ tests/test_auth/test_authentication_module.py
@@ -9,6 +9,7 @@ TODO:
 import unittest
 from unittest.mock import MagicMock
 from src.auth.authentication import AuthenticationModule
+import bcrypt
 
 class TestAuthenticationModule(unittest.TestCase):
     def setUp(self):
@@ -27,6 +28,34 @@ class TestAuthenticationModule(unittest.TestCase):
         # Test
         result = self.auth_module.authenticate_user("test_user", "test_password")
         self.assertTrue(result)
+
+    def test_authenticate_user_wrong_password(self):
+        # Mocking user_exists and get_user methods
+        self.auth_module.user_management_module.user_exists = MagicMock(return_value=True)
+        self.auth_module.user_management_module.get_user = MagicMock(return_value=self.create_mock_user())
+
+        # Mocking verify_password method
+        self.auth_module.verify_password = MagicMock(return_value=False)
+
+        # Test
+        result = self.auth_module.authenticate_user("test_user", "wrong_password")
+        self.assertFalse(result)
+
+    def test_password_verification_success(self):
+        # Mocking bcrypt.checkpw method
+        bcrypt.checkpw = MagicMock(return_value=True)
+
+        # Test
+        result = self.auth_module.verify_password("test_password", "test_hashed_password")
+        self.assertTrue(result)
+
+    def test_password_verification_failure(self):
+        # Mocking bcrypt.checkpw method
+        bcrypt.checkpw = MagicMock(return_value=False)
+
+        # Test
+        result = self.auth_module.verify_password("test_password", "test_hashed_password")
+        self.assertFalse(result)
 
     def create_mock_user(self):
         # You may want to create a mock user for testing purposes
