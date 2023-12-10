6e59a0522a4a3250107b97fb7de594dc9b74a2cc
RodrigoPintucci <rodrigoghp@gmail.com>
2023-12-09 21:30:32 -0300

    feat(schedulemanagement): add_element_to_schedule updates db schedule

--- src/schedule/schedule_management.py
+++ src/schedule/schedule_management.py
@@ -167,5 +167,6 @@ class ScheduleManagement:
         schedule = self.schedules[schedule_id]
         if element_id not in schedule.elements:
             schedule.elements = schedule.elements + [element_id]
+            self.update_schedule(schedule_id)
         else: 
             raise DuplicatedIDError(f"Element with ID {element_id} already exists in schedule {schedule_id}")
