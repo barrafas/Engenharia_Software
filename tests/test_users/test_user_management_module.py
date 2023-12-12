7237fbc395ca157f220a72f5b32060ff273d710e
RodrigoPintucci <rodrigoghp@gmail.com>
2023-12-11 23:11:19 -0300

    fix(user): user_management all tests

--- tests/test_users/test_user_management_module.py
+++ tests/test_users/test_user_management_module.py
@@ -9,6 +9,7 @@ from unittest.mock import Mock, MagicMock, patch
 from src.database.mongo_module import MongoModule, DuplicatedIDError, NonExistentIDError
 from src.calendar_elements.element_management import ElementManagement
 from src.calendar_elements.element_interface import Element
+from src.schedule.schedule_model import Schedule
 from src.schedule.schedule_management import ScheduleManagement
 import bcrypt
 
@@ -52,28 +53,6 @@ class TestUserManagementModule(unittest.TestCase):
         user_management.create_user(username, email, password, user_preferences, user_id)
         # Assert
         mock_db_module.insert_data.assert_called_once_with('users', expected_user_info)
-
-        # def mock_select_data(collection, query):
-        #     if collection == "users" and query == {"_id": "test_user1"}:
-        #         return [{"_id": "test_user1",
-        #                 "username": "test_user",
-        #                 "email": "test_email", 
-        #                 "hashed_password": "test_hashed_password"}]
-        
-        # self.user_management.db_module.select_data = MagicMock(side_effect=mock_select_data)
-        # self.user_management.db_module.insert_data = MagicMock()
-        # user_id = "test_user1"
-        # username = "test_user"
-        # email = "test_email"
-        # password = "test_password"
-        # user_preferences = {}
-        # user = User(user_id, username, email, [], password, user_preferences)
-        # self.user_management.user_exists = MagicMock(return_value=False)
-        # self.user_management.create_user(username, email, password, user_preferences, user_id)
-        
-        # self.user_management.db_module.insert_data.assert_called_once_with("users", {"_id": user_id,
-
-
 
     def test_create_existing_user(self):
         """Test that create_user raises DuplicatedIDError when the user id already exists"""
@@ -199,20 +178,65 @@ class TestUserManagementModule(unittest.TestCase):
         with self.assertRaises(NonExistentIDError):
             user_management.update_user(user_id)
 
-    def test_add_schedule_to_user(self):
-        """Test adding a schedule to a user"""
-        self.db_module.select_data = MagicMock(return_value = ['a'])
-        self.db_module.update_data = MagicMock()
-        self.user_management.user_exists = MagicMock(return_value=True)
-        self.user_management.users = {"test_id": User("test_id", "test_user",
-                                                        "test_email", [],
-                                                        "test_password")}
-        self.user_management.add_schedule_to_user("test_id", "test_schedule",
-                                                  "write")
-        self.db_module.update_data.assert_called_once_with("users", {"_id": "test_id"},
-                                {'id': 'test_id', 'username': 'test_user',
-                                'email': 'test_email', 'schedules': ["test_schedule"], 
-                                'password': "test_password", 'user_preferences': {}})
+    # def test_add_schedule_to_user(self):
+    #     """Test that add_schedule_to_user correctly adds a schedule to a user and updates the schedule's permissions"""
+    #     # Arrange
+    #     user_id = 'existing_user_id'
+    #     other_user_id = 'other_user_id'
+    #     schedule_id = 'new_schedule_id'
+    #     permission = 'read'
+    #     user_info = {'_id': user_id, 'username': 'username', 'email': 'email', 'schedules': []}
+    #     user = User(**user_info)
+    #     schedule = Schedule(schedule_id, 'title', 'description', {other_user_id: permission}, [])
+    #     #schedule.set_title = MagicMock()  # Mock set_title
+    #     mock_db_module = MagicMock()
+    #     mock_db_module.select_data.return_value = [user_info]
+    #     user_management = UserManagement(mock_db_module)
+    #     user_management.users[user_id] = user
+    #     mock_schedule_management = MagicMock()
+    #     mock_schedule_management.get_schedule.return_value = schedule
+    #     user_management.ScheduleManagement = mock_schedule_management
+
+    #     # Act
+    #     user_management.add_schedule_to_user(user_id, schedule_id, permission)
+
+    #     # Assert
+    #     self.assertIn(schedule_id, user.schedules)
+    #     self.assertEqual(schedule.permissions[user_id], permission)
+
+    def test_add_schedule_to_user_updates_user_schedules(self):
+        """Test that add_schedule_to_user updates the user's schedules"""
+        # Arrange
+        user_id = 'existing_user_id'
+        schedule_id = 'new_schedule_id'
+        permission = 'read'
+        mock_schedule = MagicMock()
+        self.user_management.users[user_id] = User(user_id, 'username', 'email', [], {})
+        user_info = {'_id': user_id, 'username': 'username', 'email': 'email', 'schedules': []}
+        self.user_management.db_module.select_data.return_value = [user_info]  # Return a list so it can be subscripted
+        with patch.object(ScheduleManagement, 'get_schedule', return_value=mock_schedule), \
+            patch.object(UserManagement, 'user_exists', return_value=True):
+            # Act
+            self.user_management.add_schedule_to_user(user_id, schedule_id, permission)
+            # Assert
+            self.assertIn(schedule_id, self.user_management.users[user_id].schedules)
+
+    def test_add_schedule_to_user_updates_schedules_permissions(self):
+        """Test that add_schedule_to_user updates the schedule's permissions"""
+        # Arrange
+        user_id = 'existing_user_id'
+        schedule_id = 'new_schedule_id'
+        permission = 'read'
+        mock_schedule = MagicMock()
+        self.user_management.users[user_id] = User(user_id, 'username', 'email', [], {})
+        user_info = {'_id': user_id, 'username': 'username', 'email': 'email', 'schedules': []}
+        self.user_management.db_module.select_data.return_value = [user_info]  # Return a list so it can be subscripted
+        with patch.object(ScheduleManagement, 'get_schedule', return_value=mock_schedule), \
+            patch.object(UserManagement, 'user_exists', return_value=True):
+            # Act
+            self.user_management.add_schedule_to_user(user_id, schedule_id, permission)
+            # Assert
+            self.assertEqual(mock_schedule.permissions[user_id], permission)
 
     def test_add_schedule_to_nonexistant_user(self):
         """Test adding a schedule to a nonexistent user"""
